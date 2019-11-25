684ebd90fbd2256afe46814d3edd92dbb9f33745
YoshiYodaFoster <noahbfoster@gmail.com>
2019-11-24 21:47:15 -0500

    starting api code

--- render.js
+++ render.js
@@ -1,16 +1,17 @@
+// const fetch = require('node-fetch')
+
 $(function () {
     renderSite();
 })
 
 let userLatitude = null;
 let userLongitude = null;
+const apiKey="pm8o9ejAV8iA0lnYN8fK4lEKdh6nVH3foW1CB76vo0kVN9IK6dqv6awLhlVSWpm81FeaXAgGyEOnycrvc6HdXlPtbcQv7vC1wvOjkJ4Ei7LLrhvH-K3xQHtxafbWXXYx"
 
 export async function renderSite() {
     const $root = $('#root')
     
     navigator.geolocation.getCurrentPosition(setPosition);
-
-    // renderOneRestaurant();
 
     document.getElementById("retryButton").addEventListener("click", function(event) {
         handleRetry()
@@ -19,7 +20,26 @@ export async function renderSite() {
 
 export async function renderOneRestaurant() {
     const $root = $('#root');
-    root.append("Longitude: " + userLongitude + " Latitude: " + userLatitude)
+    const params = new URLSearchParams();
+    params.append('latitude', userLatitude)
+    params.append('longitude', userLongitude)
+    $root.append(`<p>Longitude: ${userLongitude}, Latitude: ${userLatitude}</p>`)
+    // fetch('https://api.yelp.com/v3/businesses/search', {
+    //     method: 'GET', headers: {Authorization: apiKey, 'Access-Control-Allow-Origin':false}
+    // })
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         console.log(data)
+    //     })
+    const result = await axios({
+        method: 'get',
+        url: 'https://api.yelp.com/v3/businesses/search',
+        withCredentials: true,
+        
+    })
+    console.log(result)
+        
+
 }
 
 async function setPosition(position) {
